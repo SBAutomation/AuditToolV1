d7ec675097d4f92a3fed4e73051fb9e800fab80a
Travis Heidelberger <Travis.H.Dev@gmail.com>
2023-08-03 13:49:52 -0500

    Fetch -> SQL
    Working with SQL instead of Fetch

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const { Client } = require("pg");
 
 const app = express();
 /* The line `const PORT = process.env.PORT || 3001;` is setting the value of the `PORT` constant. */
@@ -28,44 +29,80 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
 });
 
-///model?type=makes&user=admin&pass=sunbird
-
 app.get("/api", async (req, res) => {
-    /* The line `const urlInput = req.query.type;` is extracting the value of the `type` query
-    parameter from the request URL. */
-    const urlInput = req.query.type;
-    const domain = req.query.domain;
-    const user = req.query.user;
-    const pass = req.query.pass;
+    // Replace the following with your actual database connection details
+    const config = {
+        user: "admin",
+        password: "WAT499er!",
+        host: "10.34.3.86",
+        database: "raritan",
+        port: 5432, // Replace with your PostgreSQL port if it's different
+    };
 
-    try {
-        var myHeaders = new Headers();
-        // myHeaders.append("Authorization", "Basic YWRtaW46c3VuYmlyZA==");
+    // Your SQL query
+    const query = 'SELECT * from odbc."dcModels"';
 
-        // myHeaders.append("Authorization", "Basic " + btoa(`${user}:${pass}`));
+    // Create a new PostgreSQL client
+    const client = new Client(config);
 
-        var requestOptions = {
-            method: "GET",
-            headers: myHeaders,
-            redirect: "follow",
-        };
+    // Connect to the database
+    client.connect();
 
-        const response = await fetch(
-            // `https://${domain}/api/v2/dcimoperations/search/${urlInput}/hp`,
-            "https://10.34.3.86/api/v2/dcimoperations/search/makes/hp",
-            requestOptions
-        );
-        const result = await response.text();
+    // Perform the query
+    client.query(query, (err, result) => {
+        if (err) {
+            console.error("Error executing query:", err);
+            client.end();
+            return;
+        }
 
-        res.json({ message: result });
-    } catch (error) {
-        console.log("error", error);
-        res.status(500).json({
-            error: "Something went wrong. Check the server",
-        });
-    }
+        // Close the client connection
+        client.end();
+
+        // Convert the query result to JSON and print it
+        const jsonResult = JSON.stringify(result.rows);
+        res.json({ message: jsonResult });
+    });
 });
 
+///model?type=makes&user=admin&pass=sunbird
+//----------------------------------------------------------
+// app.get("/api", async (req, res) => {
+//     /* The line `const urlInput = req.query.type;` is extracting the value of the `type` query
+//     parameter from the request URL. */
+//     const urlInput = req.query.type;
+//     const domain = req.query.domain;
+//     const user = req.query.user;
+//     const pass = req.query.pass;
+
+//     try {
+//         var myHeaders = new Headers();
+//         // myHeaders.append("Authorization", "Basic YWRtaW46c3VuYmlyZA==");
+
+//         // myHeaders.append("Authorization", "Basic " + btoa(`${user}:${pass}`));
+
+//         var requestOptions = {
+//             method: "GET",
+//             headers: myHeaders,
+//             redirect: "follow",
+//         };
+
+//         const response = await fetch(
+//             // `https://${domain}/api/v2/dcimoperations/search/${urlInput}/hp`,
+//             "https://10.34.3.86/api/v2/dcimoperations/search/makes/hp",
+//             requestOptions
+//         );
+//         const result = await response.text();
+
+//         res.json({ message: result });
+//     } catch (error) {
+//         console.log("error", error);
+//         res.status(500).json({
+//             error: "Something went wrong. Check the server",
+//         });
+//     }
+// });
+//----------------------------------------------------------
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
